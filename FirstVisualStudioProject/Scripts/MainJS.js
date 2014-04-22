d00d802d55a0184d27090a2c23e3b8ae4c241b02
andreij6 <andreij6@gmail.com>
2014-04-22 08:30:25 -0700

    First Commit - Created project

---
+++ FirstVisualStudioProject/Scripts/MainJS.js
@@ -0,0 +1,18 @@
+﻿function LetterCapitalize(str) {
+    var firstLetter;
+    var theRest;
+
+    wordsArr = str.split(" ");
+    for (var i = 0; i < wordsArr.length; i++) {
+        var firstLetter = wordsArr[i].charAt(0).toUpperCase();
+        var theRest = wordsArr[i].slice(1, wordsArr[i].length);
+
+        wordsArr[i] = firstLetter + theRest;
+    }
+    str = wordsArr.join(" ");
+    // code goes here  
+    return str;
+
+}
+
+console.log(LetterCapitalize("hello world"));
